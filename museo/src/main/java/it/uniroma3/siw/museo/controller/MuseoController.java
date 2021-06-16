31086573d98058a87e20c207887acbdac377b5df
Francesco9932 <72614773+Francesco9932@users.noreply.github.com>
2021-06-16 20:04:27 +0200

    Creazione informazioni.html, collezioni e richieste http per arrivarci

---
+++ museo/src/main/java/it/uniroma3/siw/museo/controller/MuseoController.java
@@ -0,0 +1,26 @@
+package it.uniroma3.siw.museo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import it.uniroma3.siw.museo.service.MuseoService;
+
+@Controller
+public class MuseoController {
+	@Autowired
+	private MuseoService museoService;
+	
+	@RequestMapping(value="/collezioni",method = RequestMethod.GET)
+	public String getCollezioni(Model model) {
+		model.addAttribute("collezioni",museoService.getCollezioni());
+		return "collezioni.html";
+	}
+	
+	@RequestMapping(value="/informazioni",method = RequestMethod.GET)
+	public String getInformazioni() {
+		return "informazioni.html";
+	}
+}
