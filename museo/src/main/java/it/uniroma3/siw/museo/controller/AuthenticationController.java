088ec0b51c60001df4b1218a3ea008a5081eab7c
Francesco9932 <72614773+Francesco9932@users.noreply.github.com>
2021-07-05 08:38:26 +0200

    corretto il comportamento di /logout

--- museo/src/main/java/it/uniroma3/siw/museo/controller/AuthenticationController.java
+++ museo/src/main/java/it/uniroma3/siw/museo/controller/AuthenticationController.java
@@ -1,8 +1,13 @@
 package it.uniroma3.siw.museo.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -44,8 +49,12 @@ public class AuthenticationController {
 	}
 
 	@RequestMapping(value = "/logout", method = RequestMethod.GET)
-	public String logut(Model model) {
+	public String logut(Model model, HttpServletRequest request, HttpServletResponse response) {
 		model.addAttribute("opere",museoService.getOpere());
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    if (auth != null){    
+	        new SecurityContextLogoutHandler().logout(request, response, auth);
+	    }
 		return "index.html";
 	}
 	
